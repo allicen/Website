216692b25408e35e33a65caf04feebb4bd18f7e5
Elena Rubleva <ellena.hella@yandex.ru>
2019-12-03 22:32:27 +0500

    Выведены релевантные записи в посте.

--- modules/blog.php
+++ modules/blog.php
@@ -9,10 +9,12 @@
     $breadCrumb = '<a href = "/">Главная</a> / ';
     $postCount = 0;
     $emptyCategory = 'В этой категории нет записей.';
+    $relevantPostsBlock = '';
 
     $categoryUrl = $url[2];
     $categoryUrlAdd = $url[2];
     $postUrl = $url[3];
+    $relevantPosts = '';
 
     // Вывод категорий
     if($query = mysqli_query($connect, "SELECT * FROM blog_category") and mysqli_fetch_assoc($query) !=''){
@@ -99,13 +101,32 @@
                 $categoryName = $rowUrl['name'];
                 $linkPost = $rowUrl['link'];
             }
+
+            // Вывод других записей из этой же категории
+            if($queryRelevant = mysqli_query($connect, "SELECT * FROM blog_posts WHERE category_id = '$categoryId' AND link != '$postUrl'") and mysqli_fetch_assoc($queryRelevant) !=''){
+                mysqli_data_seek($queryRelevant, 0);
+                $relevantPosts = '<ul>';
+                while($rowRelevant = mysqli_fetch_assoc($queryRelevant)){
+                    $relevantPosts .= '<li><a href="/blog/'.$linkPost.'/'.$rowRelevant['link'].'/">';
+                    $relevantPosts .= $rowRelevant['h1'];
+                    $relevantPosts .= '</a></li>';
+                }
+                $relevantPosts .= '</ul>';
+            }
+
+            if($relevantPosts != ''){
+                $relevantPostsBlock = '<div class="relevant-post"><div class="title">Еще записи из этой категории:</div><div class="list">'.$relevantPosts.'</div></div>';
+            }
+
+
             $pageName = $row['h1'];
             $date = $row['date'];
             $out = '<div class="details">
                         <div class="date">Дата: '.$date.'</div>
                         <div class="category">Рубрика: <a href="/blog/'.$linkPost.'/">'.$categoryName.'</a></div>
                     </div>'
-                .$row['text'];
+                .$row['text']
+                .$relevantPostsBlock;
             $blogPreview = '';
             $breadCrumb .= '<a href="/blog/">'.$blog.'</a> / <a href="/blog/'.$linkPost.'/">'.$categoryName.'</a> / '.$pageName;
             require_once($_SERVER['DOCUMENT_ROOT']."/templates/page.html");
