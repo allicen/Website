576e9db215b9691edb96a782035fd20c1cc4d082
Elena Rubleva <ellena.hella@yandex.ru>
2020-01-09 01:18:41 +0500

    Раздел Опции (основа).

--- backend/admin.php
+++ backend/admin.php
@@ -9,24 +9,27 @@ $url = explode('/', str_replace($delete, '/', $url));
 
 if($url[2] == ''){
     $out = 'Вы успешно авторизовались как <strong>'.$userName .'</strong>';
+}else{
+    $out = 'Вы находитесь на странице: ';
 }
 
 
 switch ($url[2]){
     case ('options'):
-        $out = 'Опции';
+        $out .= '<strong>Опции</strong>';
+        require_once($_SERVER['DOCUMENT_ROOT']."/backend/pages/options.php");
         break;
     case ('pages'):
-        $out = 'Страницы';
+        $out .= '<strong>Страницы</strong>';
         break;
     case ('blog'):
-        $out = 'Блог';
+        $out .= '<strong>Блог</strong>';
         break;
     case ('portfolio'):
-        $out = 'Портфолио';
+        $out .= '<strong>Портфолио</strong>';
         break;
     case ('files'):
-        $out = 'Файлы';
+        $out .= '<strong>Файлы</strong>';
         break;
 }
 
--- backend/pages/options.php
+++ backend/pages/options.php
@@ -1,1 +1,21 @@
 <?php
+$out .= file_get_contents($_SERVER['DOCUMENT_ROOT']."/backend/pages/form/options.html");
+
+
+require_once($_SERVER['DOCUMENT_ROOT']."/core/connect.php");
+
+if($query = mysqli_query($connect, "SELECT * FROM options") and mysqli_fetch_assoc($query) !=''){
+    mysqli_data_seek($query, 0);
+    while($row = mysqli_fetch_assoc($query)){
+        $out .= '<tr>';
+        $out .= '
+                <td>'.$row['id'].'</td>
+                <td>'.$row['name'].'</td>
+                <td>'.$row['value'].'</td>
+                <td><a href="?id='.$row['id'].'&action=edit">изменить</a></td>
+            ';
+        $out .= '</tr>';
+    }
+}
+
+$out .= '</tbody></table>';
