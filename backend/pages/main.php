7028df8d6746af072b175248972168657f785e63
Elena Rubleva <ellena.hella@yandex.ru>
2020-01-11 23:20:35 +0500

    В админке добавлено редактирование и добавление блоков на главнйо странице.

--- backend/pages/main.php
+++ backend/pages/main.php
@@ -3,10 +3,55 @@ require_once($_SERVER['DOCUMENT_ROOT']."/core/connect.php");
 $id = explode('=', $url[4])[1];
 $actionType = explode('=', $url[5])[1];
 
+$action = 'Добавление записи';
 $link = '';
 $name = '';
 $title = '';
 $text = '';
+
+
+if($url[4] == '' && $actionType != 'edit'){
+    if(isset($_POST['submit']) && $_POST['submit'] != ''){
+        $name = $_POST['name'];
+        $link = $_POST['link'];
+        $title = $_POST['title'];
+        $text = $_POST['text'];
+        if($query = mysqli_query($connect, "INSERT INTO `top_menu` (`id`, `name`, `link`, `title`, `text`) 
+                                                   VALUES (NULL, '$name', '$link', '$title', '$text')") and ($query)){
+            $info = '<div class="green info">Запись <strong>'.$name.'</strong> успешно добавлена! <a href="#'.$id.'">Посмотреть</a> или <a href="/admin/portfolio/technologies/#edit">добавить еще одну</a></div>';
+        }else{
+            $info = '<div class="red info">Запись не была добавлена! Проверьте корректность подключения к БД.</div>';
+        }
+    }
+}
+
+if($actionType == 'edit'){
+    $action = 'Редактирование записи';
+    if($query = mysqli_query($connect, "SELECT * FROM top_menu WHERE id = ".$id."") and mysqli_fetch_assoc($query) !=''){
+        mysqli_data_seek($query, 0);
+        while($row = mysqli_fetch_assoc($query)){
+            $name = $row['name'];
+            $link = $row['link'];
+            $title = $row['title'];
+            $text = $row['text'];
+        }
+    }else{
+        $info = '<div class="red info">Проверьте корректность подключения к БД.</div>';
+    }
+    if(isset($_POST['submit']) && $_POST['submit'] != ''){
+        $name = $_POST['name'];
+        $link = $_POST['link'];
+        $title = $_POST['title'];
+        $text = $_POST['text'];
+        if($query = mysqli_query($connect, "UPDATE `top_menu` SET `link` = '$link', `name` = '$name', `title` = '$title', `text` = '$text' WHERE `top_menu`.`id` = ".$id."") and ($query)){
+            $info = '<div class="green info">Запись <strong>'.$name.'</strong> успешно отредактирована! <a href="#'.$id.'">Посмотреть</a></div>';
+        }else{
+            $info = '<div class="red info">Запись не была обновлена! Проверьте корректность подключения к БД.</div>';
+        }
+    }
+}
+
+
 
 if($actionType == 'delete'){
     if($query = mysqli_query($connect, "DELETE FROM `top_menu` WHERE `top_menu`.`id` = ".$id."") and ($query)){
